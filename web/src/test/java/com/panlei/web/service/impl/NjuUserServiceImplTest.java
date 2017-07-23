58185997feb53ce3fc1f67c5ffe1753cd5d17f3a
PanLei <361panlei@163.com>
2017-07-23 17:09:14 +0800

    add:webchat

--- web/src/test/java/com/panlei/web/service/impl/NjuUserServiceImplTest.java
+++ web/src/test/java/com/panlei/web/service/impl/NjuUserServiceImplTest.java
@@ -23,7 +23,7 @@ public class NjuUserServiceImplTest {
             //Document doc = Jsoup.connect("http://en.wikipedia.org/").get();
             Connection con = Jsoup.connect("http://bbs.nju.edu.cn/vd55599/bbslogin?type=2").userAgent("Mozilla/5.0 (Windows NT 10.0; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0").timeout(3000);
             con.data("id","Jeson");
-            con.data("pw","panlei10");
+            con.data("pw","panlei110");
             con.data("lasturl","");
             Document doc = con.post(); //将获取到的内容打印出来
             String CookieString = doc.head().getElementsByIndexEquals(3).toString();
