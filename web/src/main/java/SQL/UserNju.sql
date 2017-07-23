cf157ed7d3fc9ff34fbb2fe7a665741658b667d0
Wenchao Li <wenchao.li@citrix.com>
2017-07-23 16:30:45 +0800

    Revert "add:postWritePost"
    This reverts commit 34bc4934296c25d03332e97ba92f00ebc47b149e.

--- web/src/main/java/SQL/UserNju.sql
+++ web/src/main/java/SQL/UserNju.sql
@@ -1,10 +1,13 @@
- CREATE TABLE `users` (                                                                                                                                                                                                                                                                                                                                                                              
-          `id` int(10) NOT NULL AUTO_INCREMENT,                                                                                                                                                                                                                                                                                                                                                             
-          `username` char(50) NOT NULL DEFAULT '',                                                                                                                                                                                                                                                                                                                                                          
-          `passwd` char(50) NOT NULL DEFAULT '',                                                                                                                                                                                                                                                                                                                                                            
-          `cookie` char(100) NOT NULL DEFAULT '',                                                                                                                                                                                                                                                                                                                                                           
-          `webchat_id` char(50) NOT NULL DEFAULT '',                                                                                                                                                                                                                                                                                                                                                        
-          `create_time` datetime NOT NULL,                                                                                                                                                                                                                                                                                                                                                                  
-          PRIMARY KEY (`id`),                                                                                                                                                                                                                                                                                                                                                                               
-          UNIQUE KEY `webchat_id` (`webchat_id`)                                                                                                                                                                                                                                                                                                                                                            
-        ) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8        
+CREATE TABLE `users` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `username` char(50) NOT NULL DEFAULT '',
+  `passwd` char(50) NOT NULL DEFAULT '',
+  `webchat_id` char(50) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8
+
+
+alter table users add column create_time   DATETIME NOT NULL;
+
+
